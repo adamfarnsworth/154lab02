1c5f13b7f10379cc9a907f686edc68f159ccc98f
adamfarnsworth@umail.ucsb.edu <atfarnsworth@umail.ucsb.edu>
2018-04-29 14:05:55 -0700

    logically works, but computer freezes when ran

--- atfarnsworthLab02.asm
+++ atfarnsworthLab02.asm
@@ -2,9 +2,9 @@
     #globals
     emptyArrayC: .space 8 #1 word = 4 bytes, 2 words = 8 bytes -> 64 bits
     emptyArrayD: .space 8 #1 word = 4 bytes, 2 words = 8 bytes -> 64 bits   
-    wordSet1: .word 1, 2
-    wordSet2: .word 3, 4
-.text # Assembly language instructions
+    wordSet1: .word ‭1077392179‬, ‭858993459‬ #23.7
+    wordSet2: .word ‭1078751723‬, ‭2233382994‬ #56.89
+  .text # Assembly language instructions
 
 # a0 = A[]
 # a1 = B[]
@@ -15,17 +15,22 @@
 # t2 = current word in A[2]
 # t3 = current word in B[1]
 # t4 = current word in B[2]
-# t6 = exponent difference
+# t5 = exponent difference
+# t6 = current word in A[1] with overflow
+# t7 = current word in A[2] with overflow
+# t8 = current word in B[1] with overflow
+# t9 = current word in B[2] with overflow
 # s0 = (+) or (-) final answer
 # s1 = word A[1]
 # s2 = word A[2]
 # s3 = word B[1]
 # s4 = word B[2]
-
+# s5 = right side
+# s6 = left side
 main:
 
-  la		$a0, wordSet1		        # A = first set of words
-  la		$a1, wordSet2		        # B = second set of words
+  la		$a0, wordSet1		      # A = first set of words
+  la		$a1, wordSet2		      # B = second set of words
   la		$a2, emptyArrayC	      # declairing "C"
   la		$a3, emptyArrayD	      # declairing "D" 
   lw		$s1, 0($a0)		          # $s1 = A[1]
@@ -50,58 +55,103 @@ findExponent:
     bge		$t1, $t3, aLargerThanB	  # if A^ >= B^ then target
 
 # A^ < B^
-    sub   $t6, $t3, $t1             # ^diff = B^ - A^
+# assinging exponent to C[1]
+    sll     $t5, $s3, 1                   # get rid of sign bit
+    srl     $t5, $t5, 21                  # 00..00xx..xx
+    sll     $t5, $t5, 20                  # 0x..xx00..00
+    sw		$t5, 0($a2)		              # C[1] = 0x..xx00..00 has exponent
+
+    sub   $t5, $t3, $t1             # ^diff = B^ - A^
 
     addi	$t1, $s1, 0			          # t1 = A[1]
     addi	$t2, $s2, 0			          # t2 = A[2]
     addi	$t3, $s3, 0			          # t3 = B[1]
     addi	$t4, $s4, 0			          # t4 = B[2]
+
+    addi	$t6, $s1, 0			          # t1 = A[1] #for overflow
+    addi	$t7, $s2, 0			          # t2 = A[2]
+    addi	$t8, $s3, 0			          # t3 = B[1]
+    addi	$t9, $s4, 0			          # t4 = B[2]    
 #removing |S|E10...E0|
-    sll   $t1, $t1, 12              # xx..xx00..00
-    srl   $t1, $t1, 12              # 00..00xx..xx
-    sll   $t3, $t3, 12              # xx..xx00..00
-    srl   $t3, $t3, 12              # 00..00xx..xx
+    sll     $t1, $t1, 12              # xx..xx00..00
+    srl     $t1, $t1, 12              # 00..00xx..xx
+    sll     $t3, $t3, 12              # xx..xx00..00
+    srl     $t3, $t3, 12              # 00..00xx..xx
+
+    sll     $t6, $t6, 11              # xx..xx00..00 # keeping extra bit
+    srl     $t6, $t6, 11              # 00..00xx..xx 
+    sll     $t8, $t8, 12              # xx..xx00..00
+    srl     $t8, $t8, 12              # 00..00xx..xx
 # shifting to make exponents equal
     li		$t0, 32
-    sub		$t0, $t0, $t6		          # shift = 32 - exponent difference
-    sllv  $t0, $t1, $t0             # t0 = shifted for word 2
-    sllv  $t1, $t1, $t6             # word 1 has been shifted
-    sllv  $t2, $t2, $t6             # word 2 has been shifted
-    add		$t2, $t0, $t2		          # word 2 now has shiffted bits from word 1 shift
+    sub		$t0, $t0, $t5		      # shift = 32 - exponent difference
+    sllv    $t0, $t1, $t0             # t0 = shifted for word 2
+    srlv    $t1, $t1, $t5             # word 1 has been shifted
+    srlv    $t2, $t2, $t5             # word 2 has been shifted
+    add		$t2, $t0, $t2		      # word 2 now has shiffted bits from word 1 shift
+
+    li		$t0, 31
+    sub		$t0, $t0, $t5		      # shift = 31 - exponent difference
+    sllv    $t0, $t6, $t0             # t0 = shifted for word 2
+    srlv    $t6, $t6, $t5             # word 1 has been shifted
+    srlv    $t7, $t7, $t5             # word 2 has been shifted
+    add		$t7, $t0, $t7		      # word 2 now has shiffted bits from word 1 shift
 
     j		checkSign				            # check the sign
   
-aLargerThanB:
-# A^ > B^   
-    sub   $t6, $t1, $t3             # ^diff = A^ - B^
+aLargerThanB: # A^ > B^
+# assinging exponent to C[1]
+    sll     $t5, $s1, 1                   # get rid of sign bit
+    srl     $t5, $t5, 21                  # 00..00xx..xx
+    sll     $t5, $t5, 20                  # 0x..xx00..00
+    sw		$t5, 0($a2)		              # C[1] = 0x..xx00..00 has exponent
+
+    sub     $t5, $t1, $t3                 # ^diff = A^ - B^
 
     addi	$t1, $s1, 0			          # t1 = A[1]
     addi	$t2, $s2, 0			          # t2 = A[2]
     addi	$t3, $s3, 0			          # t3 = B[1]
     addi	$t4, $s4, 0			          # t4 = B[2]
-    #removing |S|E10...E0|
-    sll   $t1, $t1, 12              # xx..xx00..00
-    srl   $t1, $t1, 12              # 00..00xx..xx
-    sll   $t3, $t3, 12              # xx..xx00..00
-    srl   $t3, $t3, 12              # 00..00xx..xx
+
+    addi	$t6, $s1, 0			          # t1 = A[1] #for overflow
+    addi	$t7, $s2, 0			          # t2 = A[2]
+    addi	$t8, $s3, 0			          # t3 = B[1]
+    addi	$t9, $s4, 0			          # t4 = B[2]  
+#removing |S|E10...E0|
+    sll     $t1, $t1, 12              # xx..xx00..00
+    srl     $t1, $t1, 12              # 00..00xx..xx
+    sll     $t3, $t3, 12              # xx..xx00..00
+    srl     $t3, $t3, 12              # 00..00xx..xx
+
+    sll     $t6, $t6, 12              # xx..xx00..00 
+    srl     $t6, $t6, 12              # 00..00xx..xx 
+    sll     $t8, $t8, 11              # xx..xx00..00 # keeping extra bit
+    srl     $t8, $t8, 11              # 00..00xx..xx
 # shifting to make exponents equal
     li		$t0, 32
-    sub		$t0, $t0, $t6		          # shift = 32 - exponent difference
-    sllv  $t0, $t3, $t0             # t0 = shifted for word 2
-    sllv  $t3, $t3, $t6             # word 1 has been shifted
-    sllv  $t4, $t4, $t6             # word 2 has been shifted
-    add		$t4, $t0, $t4		          # word 2 now has shiffted bits from word 1 shift  
+    sub		$t0, $t0, $t5		      # shift = 32 - exponent difference
+    sllv    $t0, $t3, $t0             # t0 = shifted for word 2
+    srlv    $t3, $t3, $t5             # word 1 has been shifted
+    srlv    $t4, $t4, $t5             # word 2 has been shifted
+    add		$t4, $t0, $t4		      # word 2 now has shiffted bits from word 1 shift
+
+    li		$t0, 31
+    sub		$t0, $t0, $t5		      # shift = 31 - exponent difference
+    sllv    $t0, $t8, $t0             # t0 = shifted for word 2
+    srlv    $t8, $t8, $t5             # word 1 has been shifted
+    srlv    $t9, $t9, $t5             # word 2 has been shifted
+    add		$t9, $t0, $t9		      # word 2 now has shiffted bits from word 1 shift  
 
 #___________________________________________________________________________
 # finding if there is a difference in sign
 #___________________________________________________________________________   
 checkSign:
 #getting carry bits
-    srl   $t7, $s1, 31              # 0000...000x
-    srl   $t8, $s3, 31              # 0000...000x
+    srl   $t0, $s1, 31              # 0000...000x
+    srl   $t5, $s3, 31              # 0000...000x
 
 # checking if one of the values is negative
-    bne		$t7, $t8, greaterValue	  # we have a "subraction" problem
+    bne		$t0, $t5, greaterValue	            # we have a "subraction" problem
     j		startAdd				            # both signs are the same
    
 #___________________________________________________________________________
@@ -110,53 +160,158 @@ checkSign:
 #___________________________________________________________________________  
     
 greaterValue:
-  bgt		$t1, $t3, aGb	      # A[1] > B[1]
-  bgt   $t3, $t1, bGa       # A[1] < B[1]
-  j aVSbSign                # A[1] == B[1]
-aGb:  
-  move 	$t0, $zero		      # $t0 = $zero
-  j aVSbSign
+    bgt     $t1, $t3, aGb	          # A[1] > B[1]
+    bgt     $t3, $t1, bGa             # A[1] < B[1]
+    beq		$t1, $t3, aEQb	          # A[1] == B[1]  
+aGb:
+    bgt     $t0, $t5, negBminusA	  # A is (-) A > B
+    bgt     $t5, $t0, posAminusB      # B is (-) A > B
 bGa:  
-  addi 	$t0, $zero, 1		    # $t0 = 1 
-  j aVSbSign
-checkWordTwo:
-  bgt		$t2, $t4, aGb	      # A[2] > B[2]
-  bgt   $t4, $t2, bGa       # A[2] < B[2]
-  addi 	$t0, $zero, 1		    # A[2] == B[2] and final answer is 0       <<<<<<<<<<<<< edge case were final answer is 0, put a jump here
-
-aVSbSign:
-  bgt		$t7, $t8, negA	          # A is (-)
-#B is (-)
-  bne		$t0, $zero, finalNeg	    # A<B and B is (-) then finalNeg
-  j		finalPos				            # jump to finalPos
+    bgt     $t0, $t5, posBminusA	  # A is (-) A < B
+    bgt     $t5, $t0, negAminusB      # B is (-) A < B
+aEQb:
+    bgt     $t2, $t4, aGb	          # A[2] > B[2]
+    bgt     $t4, $t2, bGa             # A[2] < B[2]
+    #j      answerIsZero		      # A[2] == B[2] and final answer is 0       <<<<<<<<<<<<< edge case were final answer is 0, put a jump here
   
-negA:
-  beq		$t0, $zero, finalNeg	    # A>B and A is (-) then finalNeg
-finalPos:
-  addi	$s0, $zero, 0			        # final answer is (+)
-  j startAdd
-finalNeg:
-  addi	$s0, $zero, 1			        # final answer is (-)
+negBminusA:
+    addi    $t0, $zero, 1		  # final answer is (-)
+    sll     $t0, $t0, 31          # 10..0000..00  
+    lw		$t5, 0($s1)		      # 0xx..xx00..00
+    add		$t0, $t0, $t5		  # 1xx..xx00..00
+    sw		$t0, 0($a2)		      # C[1] = 1xx..xx00..00
+    j       BminusA               # -(B-A)
 
+posAminusB:
+                                  # final answer is (+) 
+                                  # C[1] = 0xx..xx00..00    
+    j		AminusB			      # +(A-B)
+posBminusA:
+                                  # final answer is (+) 
+                                  # C[1] = 0xx..xx00..00    
+    j		BminusA			      # +(B-A)
+negAminusB:
+    addi    $t0, $zero, 1		  # final answer is (-)
+    sll     $t0, $t0, 31          # 10..0000..00  
+    lw		$t5, 0($s1)		      # 0xx..xx00..00
+    add		$t0, $t0, $t5		  # 1xx..xx00..00
+    sw		$t0, 0($a2)		      # C[1] = 1xx..xx00..00
+    j		AminusB			      # -(A-B)
 
-# todo: add subraction by adding a carry bit to the one that needs it
+#___________________________________________________________________________
+# Subtraction
+#___________________________________________________________________________
+# t0 = carry
+# t1 = A l/r
+# t2 = B l/r
+# t3 = final L
+# t4 = final R
+# t5 = L+R
+# t6 = A[1]
+# t7 = A[2]
+# t8 = B[1]
+# t9 = B[2]
+#___________________________________________________________________________     
+
+AminusB: 
+# subtracting RHS of A[2] and B[2]
+    sll     $t1, $t7, 16            #xx..xx00..00
+    sll     $t2, $t9, 16            #xx..xx00..00
+    srl     $t1, $t7, 16            #00..00xx..xx
+    srl     $t2, $t9, 16            #00..00xx..xx
+    sub		$t4, $t1, $t2		    # rhs = A[2]rhs - B[2]rhs
+    srl     $t0, $t4, 16            # getting carry bit
+# subtracting LHS of A[2] and B[2]
+    srl     $t1, $t7, 16            #00..00xx..xx
+    srl     $t2, $t9, 16            #00..00xx..xx
+    sub		$t3, $t1, $t2		    # lhs = A[2]lhs - B[2]lhs
+    sub		$t3, $t3, $t0		    # lhs = lhs - carry
+    srl     $t0, $t3, 16            # getting carry bit
+    sll     $t3, $t3, 16            #xx..xx00..00
+# mergeing LHS and RHS for C[2]
+    add     $t5, $t3, $t4           # lhs + rhs ===> A[2] - B[2]
+    sw		$t5, 1($a2)		        # C[2] = A[2] - B[2]
+# subtracing A[1] and B[1]
+    sub     $t5, $t6, $t8           # A[1] - B[1]
+    sub     $t5, $t5, $t0           # - carry
+    lw		$t0, 0($a2)		        # get C[1]
+    add     $t0, $t0, $t5           # adding values for C[1]
+    sw		$t0, 0($a2)		        # loading value ==> C[1]
+
+j exit
+
+BminusA:
+# subtracting RHS of A[2] and B[2]
+    sll     $t1, $t7, 16            #xx..xx00..00
+    sll     $t2, $t9, 16            #xx..xx00..00
+    srl     $t1, $t7, 16            #00..00xx..xx
+    srl     $t2, $t9, 16            #00..00xx..xx
+    sub		$t4, $t2, $t1		    # rhs = B[2]rhs - A[2]rhs
+    srl     $t0, $t4, 16            # getting carry bit
+# subtracting LHS of A[2] and B[2]
+    srl     $t1, $t7, 16            #00..00xx..xx
+    srl     $t2, $t9, 16            #00..00xx..xx
+    sub		$t3, $t2, $t1		    # lhs = B[2]lhs - A[2]lhs
+    sub		$t3, $t3, $t0		    # lhs = lhs - carry
+    srl     $t0, $t3, 16            # getting carry bit
+    sll     $t3, $t3, 16            #xx..xx00..00
+# mergeing LHS and RHS for C[2]
+    add     $t5, $t3, $t4           # lhs + rhs ===> B[2] - A[2]
+    sw		$t5, 1($a2)		        # C[2] = B[2] - A[2]
+# subtracing A[1] and B[1]
+    sub     $t5, $t8, $t6           # B[1] - A[1]
+    sub     $t5, $t5, $t0           # - carry
+    lw		$t0, 0($a2)		        # get C[1]
+    add     $t0, $t0, $t5           # adding values for C[1]
+    sw		$t0, 0($a2)		        # loading value ==> C[1]
+
+j exit
 
 #___________________________________________________________________________
 # Addition
 #___________________________________________________________________________  
+# t0 = carry
+# t1 = A l/r
+# t2 = B l/r
+# t3 = final L
+# t4 = final R
+# t5 = L+R
+# t6 = A[1]
+# t7 = A[2]
+# t8 = B[1]
+# t9 = B[2]
+#___________________________________________________________________________  
 startAdd:
-# make the word represent the exponent
-    sll   $t1, $s1, 1               # eleminate sign bit from A[1]
-    srl   $t1, $t1, 21              # 00..00xx..xx
-    sll   $t3, $s3, 1               # eleminate sign bit from B[1]
-    srl   $t3, $t3, 21              # 00..00xx..xx
+# adding RHS of A[2] and B[2]
+    sll     $t1, $t7, 16            #xx..xx00..00
+    sll     $t2, $t9, 16            #xx..xx00..00
+    srl     $t1, $t7, 16            #00..00xx..xx
+    srl     $t2, $t9, 16            #00..00xx..xx
+    add		$t4, $t1, $t2		    # rhs = A[2]rhs + B[2]rhs
+    srl     $t0, $t4, 16            # getting carry bit
+# adding LHS of A[2] and B[2]
+    srl     $t1, $t7, 16            #00..00xx..xx
+    srl     $t2, $t9, 16            #00..00xx..xx
+    add		$t3, $t1, $t2		    # lhs = A[2]lhs + B[2]lhs
+    add		$t3, $t3, $t0		    # lhs = lhs + carry
+    srl     $t0, $t3, 16            # getting carry bit
+    sll     $t3, $t3, 16            #xx..xx00..00
+# mergeing LHS and RHS for C[2]
+    add     $t5, $t3, $t4           # lhs + rhs ===> A[2] + B[2]
+    sw		$t5, 1($a2)		        # C[2] = A[2] + B[2]
+# adding A[1] and B[1]
+    add     $t5, $t6, $t8           # A[1] + B[1]
+    add     $t5, $t5, $t0           # + carry
+    lw		$t0, 0($a2)		        # get C[1]
+    add     $t0, $t0, $t5           # adding values for C[1]
+    sw		$t0, 0($a2)		        # loading value ==> C[1]
 
-# checking which exponent is larger
-    bge		$t1, $t3, aLargerThanB	  # if A^ >= B^ then target
-
+j exit
   
 
-
+exit:
+    li $v0, 10
+    syscall
 
 
    
